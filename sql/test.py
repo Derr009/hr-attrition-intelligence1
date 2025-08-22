ee9fc3c5aec8be0422868d9f6332f968d22b76f7
Shreehariballakkuraya <ballakkurayashreehari@gmail.com>
2025-08-22 17:22:46 +0530

    removed unnecessary files

--- sql/test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("data/hr_analytics.db")
-cursor = conn.cursor()
-cursor.execute("DELETE FROM merged_data;")
-conn.commit()
-conn.close()
-print("Dropped merged_data table.")
-
